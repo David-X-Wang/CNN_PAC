7512126df87115e8aa1c18a7a3512249f0e31f21
David Wang <61897610+davidwangtgs@users.noreply.github.com>
2022-02-02 19:36:03 -0600

    Add files via upload

---
+++ 2D_EEG_features/getEEGv1.m
@@ -0,0 +1,107 @@
+
+clear
+clc
+close all
+
+cd /endosome/work/TIBIR/s201302/EEGDataset/EEGdataFR1
+%This scritp computes averaged ERP from two or more electrode sites to test
+%for encoding and compares with non-encoding.
+addpath(genpath('/project/TIBIR/Lega_lab/shared/lega_ansir/shared_code/eeg_toolbox'));
+addpath(genpath('/work/TIBIR/s201302'))
+
+
+% subj_sessions = {'CC016','UT004a','UT022','UT023','UT049','UT050','UT060','UT063',...
+%     'UT064','UT067','UT069','UT074','UT081','UT090','UT095','UT105','UT108','UT112','UT113','UT117',...
+%     'UT119','UT122','UT123','UT126','UT128','UT130','UT132','UT151'};
+
+subj_sessions = {'UT008','UT009','UT011','UT012','UT013','UT014','UT015','UT016','UT017',...
+    'UT018','UT019','UT020','UT021','UT022','UT025','UT026','UT027','UT028','UT029','UT031','UT032',...
+    'UT033','UT034','UT035','UT039','UT040','UT043','UT045','UT049','UT050','UT052',...
+    'UT053','UT055','UT056','UT058','UT061','UT062','UT063','UT064','UT065','UT066','UT067','UT068',...
+    'UT087','UT088','UT090','UT092','UT093','UT095','UT096','UT097','UT108','UT116','UT121',...
+    'UT123','UT126','UT128','UT130'};
+RegionList  = {'AH-L'};
+elecList = GetChannelList(RegionList,subj_sessions);
+eleclen = cellfun(@length,elecList);
+eleclen = min(eleclen');
+acc=1;
+% for subind = 1:length(subj_sessions)
+%     Thissubj = elecList(subind,:);
+%     Thissubj = cellfun(@(x)x(1:eleclen(subind)),Thissubj,'UniformOutput',false);
+%     elecList(subind,:) = Thissubj;
+% end
+
+for rind =1:1
+    tic
+offset = 0;
+acc1= 1;
+acc2 =1;
+for subind = 1:length(subj_sessions)
+    Thissubj = subj_sessions{subind};
+    
+    
+     
+    events2load = ['/work/TIBIR/s201302/EEGDataset/FR1events/',Thissubj,'.mat'];
+    load(events2load)   
+    events = filterStruct(events,{'~strcmp(eegfile,'''')'}); %this does nothing
+          
+    for k = 1:length(events)
+        str = events(k).eegfile;
+        str_ind = strfind(str,Thissubj);
+        if ~isempty(str_ind)
+            fn = str(str_ind(2):end);
+            if exist (['/project/TIBIR/Lega_lab/shared/lega_ansir/subjFiles/' Thissubj '/eeg.noreref/' fn,'.001'],'file')==2
+            events(k).eegfile = ['/project/TIBIR/Lega_lab/shared/lega_ansir/subjFiles/' Thissubj '/eeg.noreref/' fn];
+            else 
+            events(k).eegfile = ['/project/TIBIR/Lega_lab/shared/lega_ansir/subjFiles/' Thissubj '/eeg.bipolar/' fn]; 
+            end
+        end
+    end
+        
+     Fs = 1000; %sampling frequency
+     N = 1800;    
+     encEvents = filterStruct(events,{'recalled==1'});
+     nencEvents = filterStruct(events,{'recalled==0'});     
+     
+    for Eleind = 1:length(elecList{subind,rind})
+    
+      ThisChan = elecList{subind,rind}(Eleind);    
+      [encEEG1] = gete_ms(ThisChan, encEvents, N,offset);
+      [nencEEG1] = gete_ms(ThisChan, nencEvents, N,offset);
+      encEEG1 =  resample(encEEG1',1,2)';
+      nencEEG1 = resample(nencEEG1',1,2)';
+      % normalize and notch fitler EEG
+      encEEG1 = encEEG1 - mean(encEEG1')';
+      encEEG1 = notch_mult(encEEG1);
+      encEEG1 = (encEEG1'./std(encEEG1'))';     
+      nencEEG1 = nencEEG1 - mean(nencEEG1')';
+      nencEEG1 = notch_mult(nencEEG1);
+      nencEEG1 = (nencEEG1'./std(nencEEG1'))';
+    
+
+
+        max_encpost = [];max_nencpost=[];max_encpostval=[];max_nencpostval=[];
+
+        for k = 1:length(encEvents)
+            EEG_recalled(acc1,:)= encEEG1(k,:);      
+            labelRe(acc1) = 1;             
+            acc1=acc1+1;
+        end
+
+        
+        for k = randi(length(nencEvents),1,length(encEvents))            
+            EEG_nonrecalled(acc2,:)= nencEEG1(k,:);
+            labelNR(acc2) = 0;
+            acc2=acc2+1;            
+        end
+
+    end
+    Thisfeature = [ EEG_recalled; EEG_nonrecalled;];
+    labels = [labelRe labelNR];
+    save([RegionList{rind},'-EEG.mat'],'Thisfeature')
+    save([RegionList{rind},'-labels.mat'],'labels')
+    
+end
+    toc
+    clear EEG_recalled EEG_nonrecalled labelRe labelNR
+end
---
+++ 2D_EEG_features/getPACdata.py
@@ -0,0 +1,89 @@
+
+"""
+Created on Wed Feb 10 15:45:38 2021
+
+@author: David
+"""
+#cd /work/TIBIR/s201302/QPC/CNN
+
+from scipy.io import loadmat
+import torch
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from tensorpac import Pac
+from tensorpac.signals import pac_signals_tort
+
+#%%
+
+
+# load QPC raw data and labels
+AHL = loadmat('/work/TIBIR/s201302/CNN/EEG_data/AH-L-EEG.mat')['Thisfeature']
+
+
+labels = loadmat('/work/TIBIR/s201302/CNN/EEG_data/AH-L-labels.mat')['labels'].T
+
+print('Total number of NaN entry is :',int(AHL[np.isnan(AHL)].shape[0]))
+
+Nanind =[]
+for i,k in enumerate(AHL):
+    if np.isnan(k).any():
+        Nanind.append(i)
+    if (k==0).sum()==64*64:
+        Nanind.append(i)
+
+AHL = np.delete(AHL,Nanind,0)
+labels = np.delete(labels,Nanind,0);
+
+labels = labels.reshape(len(labels))
+
+
+# num_recalled =(labels==1).sum();
+
+# AHL = AHL[:num_recalled*2,:,:]
+# labels = labels[:num_recalled*2]
+print('After elimination, total number of NaN entry is :',int(AHL[np.isnan(AHL)].shape[0]))
+
+print('Number of SuE trials is: '+str((labels==1).sum()))
+print('Number of UnsuE trials is: '+str((labels==0).sum()))
+#%%
+Fs = 500
+p = Pac(idpac=(2, 1, 3), f_pha=(1, 10, 1, .2), f_amp=(30, 100, 5, 1))
+timevec = np.linspace(0,1.8,900)
+
+AHL_recalled = AHL[np.where(labels==1)]
+AHL_Nonrecalled = AHL[np.where(labels==0)]
+
+PAC = np.empty((65,40,1))
+AHLsplit = np.array_split(AHL,200)
+for i,k in enumerate(AHLsplit):
+    pac = p.filterfit(Fs, k,verbose=None)             
+    PAC = np.concatenate((PAC,pac),axis=2)
+    
+    
+PAC = np.delete(PAC,0,2)
+
+
+# pac_recalled = p.filterfit(Fs, AHL_recalled)
+# pac_nonrecalled = p.filterfit(Fs, AHL_Nonrecalled)
+
+# pac_recalled = pac_recalled.transpose((2,0,1))
+# pac_nonrecalled = pac_nonrecalled.transpose((2,0,1))
+
+# pac_total = np.concatenate((pac_recalled,pac_nonrecalled))
+
+# p.comodulogram(PAC[:,:,:20000], cmap='Spectral_r', plotas='contour', ncontours=5,
+#                 title=r'10hz phase$\Leftrightarrow$100Hz amplitude coupling',
+#                 fz_title=14, fz_labels=13)
+
+# p.comodulogram(PAC[:,:,-20000:], cmap='Spectral_r', plotas='contour', ncontours=5,
+#                 title=r'10hz phase$\Leftrightarrow$100Hz amplitude coupling',
+#                 fz_title=14, fz_labels=13)
+# phases = p.filter(Fs, data, ftype='phase')
+# amplitudes = p.filter(Fs, data, ftype='amplitude')
+
+torch.save(PAC, 'AH-L-PAC-canolty.pt')
+# torch.save(xpac, 'AH-L-PAC.pt')
+#torch.save(labels, 'AH-L-labels.pt')  
+# torch.load('file.pt')
